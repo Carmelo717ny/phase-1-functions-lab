fe631aead996765288bab8cf6ff9926ff5774f5d
Carmelo Martinez-Vazquez <Carmelo.M.Vazquez@hotmail.com>
2022-04-06 17:09:18 -0400

    commit tree 9406209cf5b79647706b875455469527b5c4f9eb

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(amountAway) {
+    if (amountAway < 42) {
+        return 42 - amountAway}
+    else if (amountAway > 42) {
+        return amountAway - 42 
+    }
+}
+function distanceFromHqInFeet(amountAway) {
+    return distanceFromHqInBlocks(amountAway) * 264
+}
+function distanceTravelledInFeet() {
+    return distanceTravelledInFeet()
+}
